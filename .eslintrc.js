c0861229043059b07b8322a2ba351258e433d0df
Joe Fleming <joe.fleming@gmail.com>
2019-06-26 11:32:22 -0700

    chore: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ['./index.js'],
+  extends: ['./index.js', 'prettier'],
+  plugins: ['prettier'],
   parserOptions: {
     ecmaVersion: 2019,
     sourceType: 'script',
+  },
+  rules: {
+    'prettier/prettier': 'error',
   },
 };
